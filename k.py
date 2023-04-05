b0cb9b18be9318a7b3a13a6aac18585144219f44
ajaykumar1007 <ajay.kumar.3@antiersolutions.com>
2023-04-05 11:08:09 +0530

    patch release-v0.3.6

---
+++ k.py
@@ -0,0 +1,35 @@
+import re
+import subprocess
+
+# Get the name of the current branch
+current_branch = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).strip().decode('utf-8')
+
+# Check if the branch name matches the release branch format
+if re.match(r'^release-v\d+\.\d+\.\d+$', current_branch):
+    # The code is being executed in a release branch created by the preparation script
+    ...
+
+# Get the version number from the current branch name
+match = re.match(r'^release-v(\d+\.\d+\.\d+)$', current_branch)
+if match:
+    version = match.group(1)
+
+    # Check if the release tag with that version already exists
+    tags = subprocess.check_output(['git', 'tag']).strip().decode('utf-8').split('\n')
+    if f'release-v{version}' in tags:
+        # The release tag already exists
+        ...
+
+# Push the current branch to the release branch
+subprocess.check_call(['git', 'push', 'origin', current_branch])
+
+# Determine the tag name based on the current branch name
+if current_branch.endswith('-dev'):
+    tag_name = f'release-{version}-dev'
+else:
+    tag_name = f'release-{version}'
+
+# Create the tag and push it to the remote repository
+subprocess.check_call(['git', 'tag', tag_name])
+subprocess.check_call(['git', 'push', 'origin', tag_name])
+
--- n.py
+++ n.py
@@ -2,35 +2,55 @@ import re
 import subprocess
 
 # Get the name of the current branch
-current_branch = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).strip().decode('utf-8')
+try:
+    result = subprocess.run(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], capture_output=True, check=True, text=True)
+    current_branch = result.stdout.strip()
+except subprocess.CalledProcessError as e:
+    print(f"Error: {e}")
+    exit(1)
 
 # Check if the branch name matches the release branch format
-if re.match(r'^\d+\.\d+\.\d+$', current_branch):
+if re.search(r'^\d+\.\d+\.\d+$', current_branch):
     # The code is being executed in a release branch created by the preparation script
-    ...
+    print(f"Current branch '{current_branch}' is a release branch.")
+    match = re.search(r'^(\d+\.\d+\.\d+)$', current_branch)
+
+if re.search(r'^\d+\.\d+\.\d+-dev$', current_branch):
+    # The code is being executed in a release branch created by the preparation script
+    print(f"Current branch '{current_branch}' is a release branch.")
+    match = re.search(r'^(\d+\.\d+\.\d+-dev)$', current_branch)
 
 # Get the version number from the current branch name
-match = re.match(r'^(\d+\.\d+\.\d+)$', current_branch)
+#match = re.search(r'^(\d+\.\d+\.\d+-dev)$', current_branch)
+#print(match)
 if match:
     version = match.group(1)
-    print(match.group(1))
-    
+    print(f"Version number is '{version}'.")
+
     # Check if the release tag with that version already exists
-    tags = subprocess.check_output(['git', 'tag']).strip().decode('utf-8').split('\n')
-    if f'release-v{version}' in tags:
-        # The release tag already exists
-        ...
+    try:
+        result = subprocess.run(['git', 'tag'], capture_output=True, check=True, text=True)
+        tags = result.stdout.strip().split('\n')
+        if f'release-v{version}' in tags:
+            print(f"Release tag 'release-v{version}' already exists.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+        exit(1)
 
 # Push the current branch to the release branch
-subprocess.check_call(['git', 'push', 'origin', current_branch])
+try:
+    subprocess.run(['git', 'push', 'origin', current_branch], check=True)
+except subprocess.CalledProcessError as e:
+    print(f"Error: {e}")
+    exit(1)
 
-# Determine the tag name based on the current branch name
-if current_branch.endswith('-dev'):
-    tag_name = f'release-{version}-dev'
-else:
-    tag_name = f'release-{version}'
+tag_name = f'release-v{version}'
 
 # Create the tag and push it to the remote repository
-subprocess.check_call(['git', 'tag', tag_name])
-subprocess.check_call(['git', 'push', 'origin', tag_name])
+try:
+    subprocess.run(['git', 'tag', tag_name], check=True)
+    subprocess.run(['git', 'push', 'origin', tag_name], check=True)
+except subprocess.CalledProcessError as e:
+    print(f"Error: {e}")
+    exit(1)
 
